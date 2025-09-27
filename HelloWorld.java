eb8647d5a1cc415f8953f82066f3d5e8bd2e8aa9
tanuj agrawal <44617536+tanujagrawal@users.noreply.github.com>
2025-09-27 09:09:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 // Your first Java program
-public class first {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
     }
 }
+
